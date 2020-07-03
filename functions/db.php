76c593f110a975c2b99835c9d3b6a40a044057c8
latikagurnani <gurnanilatika@gmail.com>
2020-07-03 19:56:42 +0530

    index.php icons changed

--- functions/db.php
+++ functions/db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connection=mysqli_connect("localhost","JITEN","qwertyuiop","ecertificate");
+$connection=mysqli_connect("localhost","latika","Latika@1234","ecertificate");
 
 if(!$connection)
 {
