f9aa692353db703fcc46655c069095cf8584f836
janyanisanjay <sanjayjanyani43@gmail.com>
2020-07-04 01:12:58 +0530

    issue

--- functions/db.php
+++ functions/db.php
@@ -1,11 +1,7 @@
 <?php
 
 
-<<<<<<< HEAD
 $connection=mysqli_connect("localhost","Sanjay","Sanjay#123","ecertificate");
-=======
-$connection=mysqli_connect("localhost","latika","Latika@1234","ecertificate");
->>>>>>> 127c092f0ad4157fe4a8824e1de8ea490cf2e4a1
 
 if(!$connection)
 {
